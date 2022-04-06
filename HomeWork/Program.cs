aa4efcae486872b71d23ac5a0d7ee228725eafef
NoroMedafe <danya1515@inbox.ru>
2022-04-06 11:19:29 +0300

    Добавьте файлы проекта.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userZodiac;
+            string userAge;
+            Console.Write("Как вас зовут? "); userName = Console.ReadLine();
+            Console.Write("Сколько вам лет? "); userAge = Console.ReadLine();
+            Console.Write("Какой у вас знак зодиака? "); userZodiac = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {userName},вам {userAge},вы {userZodiac}");
+
+            Console.ReadKey();
+        }
+    }
+}
